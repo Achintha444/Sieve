64223e1fdddc89623ab4902005f90d8957abeabc
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-23 22:36:04 +0530

    initial commit

---
+++ lib/core/Error/Faliure.dart
@@ -0,0 +1,13 @@
+abstract class Faliure  {}
+
+//General Faliures
+
+class ServerFaliure extends Faliure {}
+
+class CacheFaliure extends Faliure {}
+
+class CountryNotFoundFaliure extends Faliure{}
+
+class InvalidInputFaliure extends Faliure{}
+
+class InternetConnectionFaliure extends Faliure{}
