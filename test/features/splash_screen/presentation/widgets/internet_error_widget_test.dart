f85954de1c4f92b3476d9bd8739be9e531c79d23
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-28 17:43:45 +0530

    internet_error_widget_test.dart done

---
+++ test/features/splash_screen/presentation/widgets/internet_error_widget_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sieve_data_privacy_app/features/splash_screen/presentation/widgets/internet_error_widget.dart';
+
+void main() {
+
+
+  Widget buildTestableWidget(Widget widget) {
+    return MediaQuery(data: MediaQueryData(), child: MaterialApp(home: widget));
+  }
+
+  testWidgets('InternetErrorWidget has the correct content', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(buildTestableWidget(InternetErrorWidget()));
+
+    final textOneFinder = find.text('NO INTERNET'.toUpperCase());
+    final textTwoFinder = find.text('connection'.toUpperCase());
+    final textThreeFinder = 
+        find.text('Connect to internet and try again'.toUpperCase());
+    final buttonFinder = find.byIcon(Icons.replay);
+
+    expect(textOneFinder, findsOneWidget);
+    expect(textTwoFinder, findsOneWidget);
+    expect(textThreeFinder, findsOneWidget);
+    expect(buttonFinder, findsOneWidget);
+  });
+}
