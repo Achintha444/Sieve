f654884d203ace028efca1bcdcfe7aa416b64978
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-31 21:51:16 +0530

    textFields is done

--- lib/features/login_screen/presentation/widgets/login_screen_widget.dart
+++ lib/features/login_screen/presentation/widgets/login_screen_widget.dart
@@ -6,166 +6,212 @@ import '../bloc/login_screen_bloc.dart';
 class LoginScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TweenAnimationBuilder(
-              tween: Tween<double>(begin: 90, end: 75),
-              duration: Duration(milliseconds: 900),
-              builder: (BuildContext context, double size, Widget child) {
-                return Text(
-                  'LOGIN'.toUpperCase(),
-                  style: TextStyle(
-                    fontSize: size,
-                    letterSpacing: 13,
-                    color: Colors.white
-                  ),
-                );
-              },
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 10,
-                bottom: 10,
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TweenAnimationBuilder(
+                tween: Tween<double>(begin: 90, end: 75),
+                duration: Duration(milliseconds: 900),
+                builder: (BuildContext context, double size, Widget child) {
+                  return Text(
+                    'LOGIN'.toUpperCase(),
+                    style: TextStyle(
+                        fontSize: size, letterSpacing: 13, color: Colors.white),
+                  );
+                },
               ),
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 10,
-                    left: 10,
-                  ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  bottom: 10,
                 ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.white,
-                    thickness: 1,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 10,
-                    left: 10,
-                  ),
-                ),
-                Text(
-                  'Login With'.toUpperCase(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 1,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 10,
-                    left: 10,
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.white,
-                    thickness: 1,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 10,
-                    left: 10,
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 10,
-                bottom: 10,
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                FloatingActionButton(
-                  heroTag: Key('google_loginscreen'),
-                  child: Container(
-                    height: 30,
-                    key: Key('google_loginscreen'),
-                    child: Image(
-                      image: AssetImage('icons/google.png'),
-                      color: Colors.white,
+              //* TextFields
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Username',
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(2.0),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
                   ),
-                  onPressed: () {
-                    _googleLoinFunction(context);
-                  },
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: 60,
-                    right: 30,
-                  ),
-                ),
-                FloatingActionButton(
-                  heroTag: Key('fb_loginscreen'),
-                  child: Container(
-                    key: Key('fb_loginscreen'),
-                    height: 50,
-                    child: Image(
-                      image: AssetImage('icons/facebook.png'),
-                      color: Colors.white,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Username',
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(2.0),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
                   ),
-                  onPressed: () {
-                    _facebookLoginFunction(context);
-                  },
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15,
-                bottom: 10,
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'DO NOT HAVE AN ACCOUNT '.toUpperCase(),
-                  style: TextStyle(
-                    letterSpacing: 0.75,
-                    color: Colors.white,
+
+              //* Dividers and Text
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 10,
+                      left: 10,
+                    ),
                   ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 10,
+                      left: 10,
+                    ),
+                  ),
+                  Text(
+                    'Login With'.toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 10,
+                      left: 10,
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 10,
+                      left: 10,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  bottom: 10,
                 ),
-                GestureDetector(
-                  child: Text(
-                    'SIGN UP'.toUpperCase(),
+              ),
+
+              //* Google and FB Login
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    heroTag: Key('google_loginscreen'),
+                    child: Container(
+                      height: 30,
+                      key: Key('google_loginscreen'),
+                      child: Image(
+                        image: AssetImage('icons/google.png'),
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      _googleLoinFunction(context);
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 60,
+                      right: 30,
+                    ),
+                  ),
+                  FloatingActionButton(
+                    heroTag: Key('fb_loginscreen'),
+                    child: Container(
+                      key: Key('fb_loginscreen'),
+                      height: 50,
+                      child: Image(
+                        image: AssetImage('icons/facebook.png'),
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      _facebookLoginFunction(context);
+                    },
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 15,
+                  bottom: 10,
+                ),
+              ),
+
+              //* Signup Navigation
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'DO NOT HAVE AN ACCOUNT '.toUpperCase(),
                     style: TextStyle(
                       letterSpacing: 0.75,
-                      color: Theme.of(context).accentColor,
+                      color: Colors.white,
                     ),
                   ),
-                  onTap: () {
-                    _signupFunction();
-                  },
-                ),
-                Text(
-                  ' NOW!'.toUpperCase(),
-                  style: TextStyle(
-                    letterSpacing: 0.75,
-                    color: Colors.white,
+                  GestureDetector(
+                    child: Text(
+                      'SIGN UP'.toUpperCase(),
+                      style: TextStyle(
+                        letterSpacing: 0.75,
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                    onTap: () {
+                      _signupFunction();
+                    },
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Text(
+                    ' NOW!'.toUpperCase(),
+                    style: TextStyle(
+                      letterSpacing: 0.75,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
