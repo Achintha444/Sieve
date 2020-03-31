4bb4ab31ae5cb75d644fa5d8791ed7d9f7476b44
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-31 17:38:27 +0530

    front end email and password validation added

--- lib/core/util/input_converter.dart
+++ lib/core/util/input_converter.dart
@@ -13,4 +13,25 @@ class InputConverter {
       return Left(InvalidInputFaliure());
     }
   }
+
+  //* For Front End Validation - Email (Presentation Logic)
+
+  bool checkEmail(String email) {
+    //* checks emails in 'anything@string.com' and 'anything@string.co.in' domains
+    String _pattern = r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$';
+    return (RegExp(_pattern).hasMatch(email));
+  }
+
+  //* For Front End Validation - Email (Presentation Logic)
+
+  bool checkPassword(String password) {
+    //* can check password 1 - Uppercase, 1- Lowercase, 1 - number , 1- Specialase
+    String _pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    return (RegExp(_pattern).hasMatch(password));
+  }
+
+  bool checkPasswordSimilar(String passwordOne, String passwordTwo){
+    return (passwordOne==passwordTwo);
+  }
 }
