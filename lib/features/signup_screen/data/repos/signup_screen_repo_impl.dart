f913918b674e0ce90dd49f980e0a9839996d273b
Achinths Isuru <achinthaisuru444@gmail.com>
2020-04-01 21:06:36 +0530

    signup_screen_repo_impl done upto now

---
+++ lib/features/signup_screen/data/repos/signup_screen_repo_impl.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:sieve_data_privacy_app/core/Platform/network_info.dart';
+
+import '../../../../core/Entities/empty_entity.dart';
+import '../../../../core/error/Faliure.dart';
+import '../../domain/repos/signup_screen_repo.dart';
+
+class SignupScreenRepoImpl extends SignupScreenRepo {
+  final NetworkInfo networkInfo;
+
+  SignupScreenRepoImpl({@required this.networkInfo});
+
+  @override
+  Future<Either<Faliure, EmptyEntity>> getSignup(
+      String email, String password) async {
+    if (await networkInfo.isConnected) {
+      print("email is " + email);
+      print("password is " + password);
+      return Right(EmptyEntity());
+    } else {
+      return Left(InternetConnectionFaliure());
+    }
+  }
+}
