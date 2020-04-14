ae8633652351194b50bbfe3ffc6b8f6255e08e50
Achinths Isuru <achinthaisuru444@gmail.com>
2020-04-14 21:44:35 +0530

    backend checker code added

--- lib/main.dart
+++ lib/main.dart
@@ -26,3 +26,83 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'dart:convert';
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Node server demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Flutter Client')),
+//         body: BodyWidget(),
+//       ),
+//     );
+//   }
+// }
+
+// class BodyWidget extends StatefulWidget {
+//   @override
+//   BodyWidgetState createState() {
+//     return new BodyWidgetState();
+//   }
+// }
+
+// class BodyWidgetState extends State<BodyWidget> {
+//   String serverResponse = 'Server response';
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return Padding(
+//       padding: const EdgeInsets.all(32.0),
+//       child: Align(
+//         alignment: Alignment.topCenter,
+//         child: SizedBox(
+//           width: 200,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: <Widget>[
+//               RaisedButton(
+//                 child: Text('Send request to server'),
+//                 onPressed: () {
+//                   _makeGetRequest();
+//                 },
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Text(serverResponse),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   _makeGetRequest() async {
+//     Response response = await post('http://10.0.2.2:3000/user/login');
+//     setState(() {
+//       print (response);
+//       serverResponse = json.decode(response.body)['email'];
+//     });
+//   }
+
+//   // String _localhost() {
+//   //   if (Platform.isAndroid)
+//   //    // return Uri.http('http://10.0.2.2:3000', '/user/login');
+//   //   else // for iOS simulator
+//   //     return 'http://localhost:3000';
+//   // }
+// }
