1454b3daf7697b7b14759210fafb5915dddf488e
Thenusan Santhirakumar <46936272+sthenusan@users.noreply.github.com>
2020-05-11 21:03:27 +0530

    dashboard check

---
+++ main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './core/Constants/theme_data.dart' as td;
+import 'features/login_screen/presentation/pages/login_screen.dart';
+import 'features/signup_screen/presentation/pages/signp_screen.dart';
+import 'features/splash_screen/presentation/pages/splash_screen.dart';
+import 'injection_container.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sieve',
+      theme: td.themeData,
+      home: SplashScreen(),
+      routes: {
+        '/loginScreen': (context) => new LoginScreen(),
+        '/signupScreen': (context) => new SignupScreen(),
+      },
+    );
+  }
+}
