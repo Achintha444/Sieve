b73fe68bffbca4d7308116579348eb873182e731
Achinths Isuru <achinthaisuru444@gmail.com>
2020-05-25 01:32:47 +0530

    alignment changed

--- lib/features/dashboard/presentation/widgets/dashboard_widget.dart
+++ lib/features/dashboard/presentation/widgets/dashboard_widget.dart
@@ -6,18 +6,15 @@ import '../../../login_screen/domain/entities/login_user.dart';
 import '../../domain/entities/dapp.dart';
 import 'dapp_card.dart';
 
-
 class DashboardWidget extends StatelessWidget {
   final LoginUser user;
   final List<Dapp> dapp;
 
   const DashboardWidget({Key key, @required this.user, @required this.dapp})
-      : super(
-      key: key);
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       drawer: DrawerDesign(
         user: user,
@@ -25,93 +22,82 @@ class DashboardWidget extends StatelessWidget {
       appBar: AppBarDesign(
         title: 'Dashboard',
         imageUrl: user.imageUrl,
-
-
       ),
       body: SingleChildScrollView(
         child: Column(
-
           children: <Widget>[
             //* Dashboard
             Padding(
-              padding: const EdgeInsets.only(
-                  top: 3.0, left: 15),
+              padding: const EdgeInsets.only(top: 3.0, left: 15,bottom: 3),
               child: Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
                   "DATA PRIVACY INTRUSEIVENESS"
-                      "\n",
+                  "\n",
                   textAlign: TextAlign.start,
                   style: TextStyle(
-                    fontSize: 18,
-                    color: Theme.of(context).primaryColor,
-                    letterSpacing: 1,
-                    fontWeight: FontWeight.w500
-
-                  ),
+                      fontSize: 18,
+                      color: Theme.of(context).primaryColor,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.w500),
                 ),
-
-
               ),
-
             ),
-
 
             Column(
               children: List.generate(
                 this.dapp.length,
-                    (index) {
-                  return Column(children: _check_cat(index));
+                (index) {
+                  return Column(children: _check_cat(index, context));
                 },
               ),
             ),
             //* More Information
             Padding(
-              padding: const EdgeInsets.only(
-                  top: 1.0, left: 20),
+              padding: const EdgeInsets.only(top: 1.0, left: 20),
               child: Align(
                 alignment: Alignment.centerLeft,
-
               ),
             ),
-
           ],
         ),
       ),
     );
   }
 
-
-  List<Widget> _check_cat(int index){
-    if ((index==0) || (dapp[index].categoryName!=dapp[index-1].categoryName)) {
-      Text cat=Text(
-        dapp[index].categoryName+" category",
-        textAlign: TextAlign.end,
-        style: TextStyle(
-          fontSize: 17,
-          color: Color(0xff265699),
-          fontWeight: FontWeight.bold,
-          letterSpacing: 1,
-
+  List<Widget> _check_cat(int index, BuildContext context) {
+    if ((index == 0) ||
+        (dapp[index].categoryName != dapp[index - 1].categoryName)) {
+      Padding cat = Padding(
+        padding: EdgeInsets.only(left: 15),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            dapp[index].categoryName + " Category",
+            //textAlign: TextAlign.end,
+            style: TextStyle(
+              fontSize: 17,
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+            ),
+          ),
         ),
       );
-      DappCard dcard=DappCard(
+
+      DappCard dcard = DappCard(
           aName: this.dapp[index].getAppName,
           cName: this.dapp[index].getCategoryName,
           iconImage: this.dapp[index].getIconImage,
-          dCount: this.dapp[index].getDCount
-      );
-      return [cat,dcard];
-    }
- else{
-      DappCard dcard= DappCard(
+          dCount: this.dapp[index].getDCount);
+      return [cat, dcard];
+    } else {
+      DappCard dcard = DappCard(
           aName: this.dapp[index].getAppName,
           cName: this.dapp[index].getCategoryName,
           iconImage: this.dapp[index].getIconImage,
-          dCount: this.dapp[index].getDCount
-      );
+          dCount: this.dapp[index].getDCount);
       return [dcard];
-      
     }
   }
 }
