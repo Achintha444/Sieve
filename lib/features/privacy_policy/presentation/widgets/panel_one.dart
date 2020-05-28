ec7dd85a859661b2b58641a46bc7a0228a5ae88e
Thuvarahan <thuvarahan97@gmail.com>
2020-05-28 13:49:17 +0530

    Update panel_one.dart

--- lib/features/privacy_policy/presentation/widgets/panel_one.dart
+++ lib/features/privacy_policy/presentation/widgets/panel_one.dart
@@ -7,43 +7,78 @@ class PanelOne extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: List.generate(
-        list.length,
-        (index) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                  left: 0,
-                  right: 15,
-                  top: 5,
-                  bottom: 10
-                ),
-                child: Icon(
-                  Icons.lens,
-                  size: 10,
-                  color: Theme.of(context).primaryColor.withOpacity(0.9),
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  list[index],
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Theme.of(context).primaryColor,
+    if (list.length > 0) {
+      return ListView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: List.generate(
+          list.length, (index) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(
+                      right: 15,
+                      top: 5
                   ),
-                  textAlign: TextAlign.justify,
+                  child: Icon(
+                    Icons.lens,
+                    size: 10,
+                    color: Theme.of(context).primaryColor.withOpacity(0.9),
+                  ),
                 ),
+                Expanded(
+                  child: Text(
+                    list[index],
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                Builder(
+                  builder: (context) {
+                    if (index < list.length - 1) {
+                      return Padding(
+                          padding: EdgeInsets.only(
+                              bottom: 30
+                          ));
+                    } else {
+                      return Container(
+                        height: 0,
+                        width: 0,
+                      );
+                    }
+                  },
+                )
+              ],
+            );
+          },
+        ),
+      );
+    } else {
+      return Container(
+        padding: EdgeInsets.only(
+            top: 10,
+            bottom: 10
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "No details available!",
+              style: TextStyle(
+                fontSize: 22,
+                color: Theme.of(context).errorColor,
+                fontWeight: FontWeight.w800,
               ),
-            ],
-          );
-        },
-      ),
-    );
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
