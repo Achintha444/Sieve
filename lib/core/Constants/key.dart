e61237803af8ad1376c771e75fb9088c863f7ed3
Achinths Isuru <achinthaisuru444@gmail.com>
2020-06-29 15:00:46 +0530

    api location folder

--- lib/core/Constants/key.dart
+++ lib/core/Constants/key.dart
@@ -1,5 +1,5 @@
 const String AUTO_LOGIN_USER_KEY = 'auto_login_user';
 const String LOGIN_TYPE = 'login_type';
-//const API_URL = 'https://sieve-data-privacy-app.herokuapp.com/app';
+const API_URL = 'https://sieve-data-privacy-app.herokuapp.com/app';
 
-const API_URL = 'http://10.0.2.2:8000/app';
+//const API_URL = 'http://10.0.2.2:8000/app';
